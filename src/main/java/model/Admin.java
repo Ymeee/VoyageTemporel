4cfd035f20b99c8fce2ea9b39269cc936b99b15b
Oumaima AZAMI <oumaima_azami@hotmail.com>
2022-10-14 14:24:21 +0100

    modif erreur

--- src/main/java/model/Admin.java
+++ src/main/java/model/Admin.java
@@ -1,6 +1,7 @@
 package model;
 
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
